b8d2995d84d71af2bdd67f0a031f4253640cfcb6
Lars Blomgren <lars3838@gmail.com>
2022-11-13 21:15:15 +0100

    Mainfil

---
+++ src/model/Main.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Main {
+    public static void main(String[] args) {
+        new Main();
+    }
+    public Main() {
+        System.out.println("model.Main");
+    }
+}
